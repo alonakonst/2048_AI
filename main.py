7d4b372a237fe34069ce52e2730374c9fefc1022
Alona Konstantinova <alona@ruc.dk>
2024-03-03 13:50:53 +0100

    initial setup

--- main.py
+++ main.py
@@ -2,20 +2,31 @@ import pygame
 
 pygame.init()
 
+#initial setup: screen size, game caption, predetermined speed, font size
 screen_width = 300
 screen_height = 400
-
 screen = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.display.set_caption('2048')
+timer = pygame.time.Clock()
+fps = 60
+
+font_name = 'freesansbold.ttf'
+font_size = 24
+font = pygame.font.Font(font_name, font_size)
 
 
 # Main loop
 run = True
 while run:
+    timer.tick(fps)
+    screen.fill('gray')
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-
+    pygame.display.flip()
 
 
 pygame.quit()
